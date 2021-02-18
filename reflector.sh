c883db9f9f3c460e30e0460c22fb0cef997d9d71
Mohammed Alhoussainy <mohammed.houssain.n@gmail.com>
2021-02-18 10:50:28 +0300

    Fix some errors

--- reflector.sh
+++ reflector.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-#reflector.sh is a script to help you update mirrorlist
-#written by:Mohammed Alhoussainy
-#
-#upgrade your system
+: 'reflector.sh is a script to help you update mirrorlist
+ written by:Mohammed Alhoussainy
+
+ upgrade your system
+ '
+export m1 m2 m3
 arch_upgrade()
 {
 sudo pacman -Syu
@@ -17,35 +19,44 @@ b="وجد الملف "
 c="تم حذف الملف"
 d="تم حفظ المرايا بنجاح "
 d1="للاسف فشل حفظ المرايا وهناك خطأ ما. "
-elif [ "$loc" == 'LANG=en_US.UTF-8' ]
-then
-a="Upgrading start"
-b="Found"
-c="File deleted"
-d="Mirrors saved successfully"
-d1="Something wrong , mirrors not saved"
+m1="يجب ادخال عدد المرايا التي تريدها"
+m2="استعمال السكربت:sh reflector.sh n  حيث n عدد المرايا"
+m3="حاول مرة اخرى"
 else
 a="Upgrading Start"
 b="found"
 c="file deleted"
 d="Mirrors saved successfully"
 d1="Something wrong , mirrors not saved"
+m1="You should enter the nomber of mirros"
+m2="Script usage : sh reflector.sh n , n= number of mirrors"
+m3="try again later"
 fi
 }
 locale_function
-#The new command enable the user choosing the nomber of mirrors
-#Also useing HTTPS & HTTP only
-#If you need rsync delete "--protocol https --protocol http"
-#CHEERS
+:'The new command enable the user choosing the nomber of mirrors
+ Also useing HTTPS & HTTP only
+ If you need rsync delete "--protocol https --protocol http"
+ CHEERS
+'
+if [ -z "$1" ] ; then
+echo "$m1"
+sleep 1
+echo "$m2"
+echo "$m3"
+sleep 1
+exit
+fi
  sudo reflector --verbose --latest $1 --protocol https --protocol http --sort rate --save /etc/pacman.d/mirrorlist
- if [ $? -eq 0 ];then
+ if [ $? -eq 0 ]
+ then
   echo "  "
-  echo $d
+  echo "$d"
   echo "  "
     FILE=/var/lib/pacman/db.lck
 if test -f "$FILE"; then
     echo -e "\e[40;38;5;82m $b \e[30;48;5;82m $FILE \e[0m"
-    #echo "FOUND $FILE "
+    # File will be removed
     sudo rm "$FILE"
     echo "$FILE $c"
     arch_upgrade
@@ -56,10 +67,10 @@ if test -f "$FILE"; then
 fi
   else
    echo "  "
-   echo $d1
+   echo "$d1"
    echo "  "
    echo "No UPGREADE NEEDED"
    fi
-  
+ 
 
 
